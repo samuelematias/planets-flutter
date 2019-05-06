3099ae6072bd09cd0fd3d7bde075fd645f41ac2e
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Pro.local>
2019-05-06 12:02:23 -0300

    implemeting the appBat without appBar class and with gradient effect.

--- lib/ui/home/GradientAppBar.dart
+++ lib/ui/home/GradientAppBar.dart
@@ -9,15 +9,32 @@ class GradientAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
     return new Container(
-      height: barHeight,
-      decoration: new BoxDecoration(color: Colors.blue),
+      padding: new EdgeInsets.only(top: statusBarHeight),
+      height: statusBarHeight + barHeight,
       child: new Center(
         child: new Text(
           title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+            fontSize: 36.0
+          ),
         ),
       ),
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(      colors: [
+          const Color(0xFF3366FF),
+          const Color(0xFF00CCFF),
+        ],
+            begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(1.0, 0.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp
+        )
+      )
     );
   }
 }
