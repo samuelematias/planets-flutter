10c0b9266e486c44899d770a1fe9a8c2a1567509
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Pro.local>
2019-05-06 18:10:15 -0300

    adding scrollview and Slivers.

--- lib/ui/home/home_page_body.dart
+++ lib/ui/home/home_page_body.dart
@@ -2,11 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:planets_flutter/ui/home/PlanetRow.dart';
 import 'package:planets_flutter/model/planets.dart';
 
-
-class HomePageBody extends StatelessWidget{
-
+class HomePageBody extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return new PlanetRow(planets[2]);
+  Widget build(BuildContext context) {
+    return new Expanded(
+      child: new Container(
+        color: new Color(0xFF736AB7),
+        child: new CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: <Widget>[
+            new SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              sliver: new SliverFixedExtentList(
+                itemExtent: 162.0,
+                delegate: new SliverChildBuilderDelegate(
+                      (context, index) => new PlanetRow(planets[index]),
+                  childCount: planets.length,
+
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
