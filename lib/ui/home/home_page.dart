fa1745095162156dda1ce2c9035e26f131f97015
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Pro.local>
2019-05-06 15:37:50 -0300

    acreating planetCard.

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:planets_flutter/ui/home/GradientAppBar.dart';
+import 'package:planets_flutter/ui/home/home_page_body.dart';
 
-class HomePage extends StatelessWidget{
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Column(
-        children: <Widget>[
-          new GradientAppBar("treva")
-        ],
-      )
-    );
+        body: new Column(
+      children: <Widget>[
+        new GradientAppBar("treva"),
+        new HomePageBody(),
+        new HomePageBody(),
+        new HomePageBody(),
+      ],
+    ));
   }
 }
---
+++ lib/ui/home/home_page_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:planets_flutter/ui/home/PlanetRow.dart';
+
+
+class HomePageBody extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return new PlanetRow();
+  }
+}
